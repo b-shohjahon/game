228a35196fd64a1698dec3c5ad4de37d1c516e2f
Shohjahon Berdiyorov <berdiyorovsh@gmail.com>
2023-01-26 23:16:07 +0500

    oyna qo'shildi

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Salom");
+    }
+}
+class Panel extends JFrame{
+    public Panel(){
+        this.show();
     }
 }
